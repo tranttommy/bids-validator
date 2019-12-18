34caa45e55936c8afabe7f4ec1c98dec234a170d
Goldmund <matthewtzito@gmail.com>
2019-12-18 14:03:29 -0800

    rm sort

--- bids-validator/tests/bids.spec.js
+++ bids-validator/tests/bids.spec.js
@@ -125,13 +125,13 @@ describe('BIDS example datasets ', function() {
       var warnings = issues.warnings
       assert(summary.sessions.length === 0)
       assert(summary.subjects.length === 1)
-      expect(summary.subjectMetadata.sort()).toEqual([
+      expect(summary.subjectMetadata.toEqual([
           {
             participantId: '01',
             age: 25,
-            sex: 'M',
+            sex: 'M'
           }
-        ].sort())
+        ])
       assert.deepEqual(summary.tasks, ['rhyme judgment'])
       assert.isFalse(summary.dataProcessed)
       assert(summary.modalities.includes('T1w'))
